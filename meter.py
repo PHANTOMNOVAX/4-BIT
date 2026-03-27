a23efafcbb9ebc5b93e7a9e16d8e38dedc3fe601
PHANTOMNOVAX <pranavshekarc@gmail.com>
2026-03-27 12:39:16 +0530

    Refactor detect_meter function for line-based syllable counting

--- meter.py
+++ meter.py
@@ -2,18 +2,24 @@ from syllable_splitter import split_syllables
 
 def detect_meter(text):
 
-    syllables = split_syllables(text)
+    lines = text.split("\n")
 
-    count = len(syllables)
+    syllable_counts = []
 
-    if count == 8:
-        return "Anushtubh"
+    for line in lines:
+        syllables = split_syllables(line)
+        syllable_counts.append(len(syllables))
 
-    elif count == 11:
-        return "Trishtubh"
+    avg = sum(syllable_counts) / len(syllable_counts)
 
-    elif count == 12:
-        return "Jagati"
+    if avg <= 8:
+        return "Anushtubh (8 syllables per line)"
+
+    elif avg <= 11:
+        return "Trishtubh (11 syllables per line)"
+
+    elif avg <= 12:
+        return "Jagati (12 syllables per line)"
 
     else:
         return "Unknown Meter"
