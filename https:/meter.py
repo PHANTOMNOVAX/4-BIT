964a5e71856833853be1be8ca70cc85257834381
PHANTOMNOVAX <pranavshekarc@gmail.com>
2026-03-27 10:50:05 +0530

    Add meter detection function based on syllable count

---
+++ https:/meter.py
@@ -0,0 +1,19 @@
+from syllable_splitter import split_syllables
+
+def detect_meter(text):
+
+    syllables = split_syllables(text)
+
+    count = len(syllables)
+
+    if count == 8:
+        return "Anushtubh"
+
+    elif count == 11:
+        return "Trishtubh"
+
+    elif count == 12:
+        return "Jagati"
+
+    else:
+        return "Unknown Meter"
