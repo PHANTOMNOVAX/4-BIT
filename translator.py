7545618d9ccfe4e9a30995ebff64f3ecd321e1ff
PHANTOMNOVAX <pranavshekarc@gmail.com>
2026-03-27 11:55:55 +0530

    Update translator.py

--- translator.py
+++ translator.py
@@ -3,5 +3,8 @@ from googletrans import Translator
 translator = Translator()
 
 def translate_text(text):
-    translated = translator.translate(text, src='sa', dest='en')
-    return translated.text
+    try:
+        translated = translator.translate(text, src='sa', dest='en')
+        return translated.text
+    except:
+        return "Translation temporarily unavailable"
