6644766e025b0ade7e657d7fc1f1023fb58b20f8
PHANTOMNOVAX <pranavshekarc@gmail.com>
2026-03-27 14:57:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request
+
 from meter import detect_meter
 from translator import translate_text
 from tts import generate_audio
 
 app = Flask(__name__)
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -11,23 +13,30 @@ def home():
     meter = None
     translation = None
     audio = None
+    lines = None
 
     if request.method == "POST":
 
         verse = request.form.get("verse", "").strip()
 
-        meter = detect_meter(verse)
+        if verse:
 
-        translation = translate_text(verse)
+            lines = verse.split("\n")
 
-        audio = generate_audio(verse)
+            meter = detect_meter(verse)
+
+            translation = translate_text(verse)
+
+            audio = generate_audio(verse, meter)
 
     return render_template(
         "index.html",
         meter=meter,
         translation=translation,
-        audio=audio
+        audio=audio,
+        lines=lines
     )
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
