f20ec87d922c90ec7649a2d53bc369e8a739591a
PHANTOMNOVAX <pranavshekarc@gmail.com>
2026-04-01 14:50:03 +0530

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,14 +1,39 @@
 from deep_translator import GoogleTranslator
+import re
+
+
+def normalize(text):
+
+    text = text.replace("।", "")
+    text = text.replace("॥", "")
+    return text.strip()
+
+
+def split_words(text):
+
+    return re.findall(r'[\u0900-\u097F]+', text)
 
 
 def translate_text(text):
-    try:
-        translation = GoogleTranslator(
-            source="auto",
-            target="en"
-        ).translate(text)
 
-        return translation
+    text = normalize(text)
 
-    except Exception:
-        return "Translation unavailable right now."
+    words = split_words(text)
+
+    translated = []
+
+    translator = GoogleTranslator(source="auto", target="en")
+
+    for word in words:
+
+        try:
+
+            result = translator.translate(word)
+
+            translated.append(result)
+
+        except:
+
+            translated.append(word)
+
+    return " ".join(translated)
